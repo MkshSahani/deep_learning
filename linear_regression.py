340c8005de8e32a4ed67bdbe83c737466958af11
Mukesh Sahani <52211262+MkshSahani@users.noreply.github.com>
2019-12-21 15:37:44 +0530

    update linear_regression.py
    updating the comments in the code 
    now, code contain the some well explained comments, about what function do.

--- linear_regression.py
+++ linear_regression.py
@@ -1,13 +1,22 @@
-# linear regression version : 1.o.
+# Linear Regression .
+
+# B = B0 + B1 * X1 + B2*X2 + B3*X3 + ... + BNXN 
+
 import numpy as np
-import time 
 import matplotlib.pyplot as plt 
+###################  linear regression forward pass #################
 
+# function name : forward_pass 
+## weights => dict parameter - 'W' : weights and 'B' : bais
+## X_batch : x input => measured
+## Y_batch : y output => mesured 
+## returns the tuple of the loss ad 
+
+# just do product the input and output and find the 
 def forward_linear_regression(x_batch, y_batch, weights):
-    # x_bath : ndarrray => matrix input 
-    # y_bath : measured output => matrix measured output
-    # weights : parameters 
-
+    # linear regression forward pass return dictionary forward_pass = {} and loss value
+    # forward_pass contain the ,measured input  and output,weights result without base added,
+    # predicted value.
     N = np.dot(x_batch, weights['W'])
     P = N + weights['B']
 
@@ -20,6 +29,14 @@ def forward_linear_regression(x_batch, y_batch, weights):
     forward_info['Y'] = y_batch 
 
     return forward_info, loss 
+
+
+####################   linear regressiono forward pass ################
+
+# function name : loss gradient 
+## forward pass : contain the data from the forward linear regression function 
+## weights : contain the data of the weights 
+## return data dictionary contain loss 
 
 def loss_gradient(forward_info, weights):
     batch_size = forward_info['X']
@@ -41,10 +58,12 @@ def loss_gradient(forward_info, weights):
 
     return loss_gradients 
 
+
+####################  train function #########################
 def train(X_train, Y_train, learning_rate = 0.001, batch_size = 23, return_weight = True, sedd = 80718, epoch = 2000):
-    data = X_train.T.shape
+    data = X_train.shape
     weights = {}
-    weights['W'] = np.ones(data)
+    weights['W'] = np.ones((data[1], 1))
     weights['B'] = np.ones((1, data[1]))
     for i in range(0, epoch):
         forward_info,loss =  forward_linear_regression(X_train, Y_train,weights)
@@ -55,11 +74,17 @@ def train(X_train, Y_train, learning_rate = 0.001, batch_size = 23, return_weigh
             weights[key] -= learning_rate*loss_grads[key]
 
     if return_weight == True:
+        print("this is weights")
+        print(weights['W'])
+        print("weight matrix")
         return weights
 
+    
+################# predict function ###########################
 def predict(X_input, weights):
     # print(X_input)
     N = np.dot(X_input, weights['W'])
+    
     return N + weights['B']
 
 if __name__ == '__main__':
@@ -69,7 +94,10 @@ if __name__ == '__main__':
         input_list.append(ma)
     output_list = []
     for i in range(0, 10):
-        ma = [3*i+7]
+        if i % 2 == 0:
+            ma = [3*i+5]
+        else:
+            ma = [3*i - 5]
         output_list.append(ma)
     print(input_list)
     print(output_list)
@@ -78,7 +106,7 @@ if __name__ == '__main__':
     print(y_train.shape)
     train_info = train(x_train, y_train)
     input_data = []
-    for i in range(11, 40):
+    for i in range(0, 10):
         ma = [i]
         input_data.append(ma)
     x_input = np.array([input_data])
@@ -86,8 +114,8 @@ if __name__ == '__main__':
     print(do[0,0:,0])
     print(x_train.flatten())
     print(y_train.flatten())
-    plt.plot(x_train.flatten(), y_train.flatten(), 'g')
-    plt.plot(x_input.flatten(), do[0,0:,0], 'r')
+    plt.scatter(x_train.flatten(), y_train.flatten(), marker = 'o')
+    plt.plot(x_input.flatten(), do[0,0:,0], 'b')
     plt.show()
 
     
