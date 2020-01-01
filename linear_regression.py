9da67f4ca41fddabea4f8a2276ea7c09cc8d1cc7
Mukesh Sahani <52211262+MkshSahani@users.noreply.github.com>
2020-01-01 12:03:55 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,121 +1,82 @@
-# Linear Regression .
-
-# B = B0 + B1 * X1 + B2*X2 + B3*X3 + ... + BNXN 
+#!/usr/bin/python3
+## linearregreesion.py 
 
 import numpy as np
-import matplotlib.pyplot as plt 
-###################  linear regression forward pass #################
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# function name : forward_pass 
-## weights => dict parameter - 'W' : weights and 'B' : bais
-## X_batch : x input => measured
-## Y_batch : y output => mesured 
-## returns the tuple of the loss ad 
-
-# just do product the input and output and find the 
-def forward_linear_regression(x_batch, y_batch, weights):
-    # linear regression forward pass return dictionary forward_pass = {} and loss value
-    # forward_pass contain the ,measured input  and output,weights result without base added,
-    # predicted value.
+## forward pass process
+def forward_pass(x_batch, y_batch, weights):
     N = np.dot(x_batch, weights['W'])
-    P = N + weights['B']
+    P = N +  weights['B']
 
     loss = np.mean(np.power(y_batch - P, 2))
 
     forward_info = {}
-    forward_info['X'] = x_batch 
+    forward_info['X'] = x_batch
+    forward_info['Y'] = y_batch
     forward_info['N'] = N 
     forward_info['P'] = P 
-    forward_info['Y'] = y_batch 
 
-    return forward_info, loss 
+    return forward_info, loss
 
+## BackWard pass to get the loss due to chage in the weights 
+def loss_gradients(forward_info, weights):
 
-####################   linear regressiono forward pass ################
-
-# function name : loss gradient 
-## forward pass : contain the data from the forward linear regression function 
-## weights : contain the data of the weights 
-## return data dictionary contain loss 
-
-def loss_gradient(forward_info, weights):
-    batch_size = forward_info['X']
     dLdP = -2*(forward_info['Y'] - forward_info['P'])
+    dPdB = np.ones_like(weights['B'])
     dPdN = np.ones_like(forward_info['N'])
-    dPdB = np.ones_like(weights['B']) 
-
-    dLdN = dLdP * dPdN 
-
     dNdW = np.transpose(forward_info['X'], (1, 0))
-
+    dLdN = dLdP*dPdN 
     dLdW = np.dot(dNdW, dLdN)
+    dLdB = (dLdP*dPdB).sum(axis = 0)
+    loss_gradient = {}
+    loss_gradient['W'] = dLdW 
+    loss_gradient['B'] = dLdB 
+    return loss_gradient
 
-    dLdB = (dLdP * dPdB).sum(axis = 0)
-
-    loss_gradients = {}
-    loss_gradients['W'] = dLdW
-    loss_gradients['B'] = dLdB 
-
-    return loss_gradients 
-
-
-####################  train function #########################
-def train(X_train, Y_train, learning_rate = 0.001, batch_size = 23, return_weight = True, sedd = 80718, epoch = 2000):
-    data = X_train.shape
+def train(x_train, y_train, learning_rate = 0.001, return_weights = True, epoch = 2000):
+    shape_ = x_train.shape
     weights = {}
-    weights['W'] = np.ones((data[1], 1))
-    weights['B'] = np.ones((1, data[1]))
-    for i in range(0, epoch):
-        forward_info,loss =  forward_linear_regression(X_train, Y_train,weights)
-        loss_grads = loss_gradient(forward_info, weights)
-        # print(loss_grads['W'])
-        # time.sleep(0.1)
+    weights['W'] = np.random.rand(shape_[1],1)
+    weights['B'] = np.random.rand(shape_[1])
+    for i in range(0, 2000):
+        forward_info,loss = forward_pass(x_train, y_train, weights)
+        loss_grad = loss_gradients(forward_info, weights)
+
         for key in weights.keys():
-            weights[key] -= learning_rate*loss_grads[key]
-
-    if return_weight == True:
-        print("this is weights")
-        print(weights['W'])
-        print("weight matrix")
-        return weights
-
+            weights[key] -= learning_rate * loss_grad[key]
     
-################# predict function ###########################
-def predict(X_input, weights):
-    # print(X_input)
-    N = np.dot(X_input, weights['W'])
-    
-    return N + weights['B']
+    train_info = [weights, return_weights]
+    return train_info
+
+def predict(x_input, weights):
+    N = np.dot(x_input, weights['W'])
+    P = N + weights['B']
+    return P
+
 
 if __name__ == '__main__':
-    input_list = []
-    for i in range(0, 10):
-        ma = [i]
-        input_list.append(ma)
-    output_list = []
-    for i in range(0, 10):
-        if i % 2 == 0:
-            ma = [3*i+5]
-        else:
-            ma = [3*i - 5]
-        output_list.append(ma)
-    print(input_list)
-    print(output_list)
-    x_train = np.array(input_list)
-    y_train = np.array(output_list)
-    print(y_train.shape)
-    train_info = train(x_train, y_train)
-    input_data = []
-    for i in range(0, 10):
-        ma = [i]
-        input_data.append(ma)
-    x_input = np.array([input_data])
-    do = predict(x_input, train_info)
-    print(do[0,0:,0])
-    print(x_train.flatten())
-    print(y_train.flatten())
-    plt.scatter(x_train.flatten(), y_train.flatten(), marker = 'o')
-    plt.plot(x_input.flatten(), do[0,0:,0], 'b')
+    data_csv = pd.read_csv('data_linear_regression.csv')
+    x_input = data_csv.iloc[:,0].values
+    plot_input = x_input
+    x_input = np.array(x_input)
+    x_input = x_input.reshape(x_input.shape[0], 1)
+    print(x_input)
+    y_output = data_csv.iloc[:,1].values 
+    plot_output = y_output
+    y_output = np.array(y_output)
+    y_output = y_output.reshape(y_output.shape[0], 1)
+    print(y_output)
+    train_info = train(x_input, y_output)
+    print(train_info)
+    predicted_value = predict([2], train_info[0])
+    print(predicted_value)
+    predicted_data = [i for i in range(0, 11)]
+    label_predict = predicted_data 
+    predicted_data = [[i] for i in predicted_data]
+    predicted_data = np.array(predicted_data)
+    predict_values = predict(predicted_data, train_info[0])
+    plt.scatter(plot_input, plot_output)
+    plt.plot(predicted_data, predict_values.flatten(), 'b')
     plt.show()
-
-    
