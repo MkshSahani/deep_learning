81c7e38d6b03c67c11ccb9dd419e95a53c84bf0d
Mukesh Sahani <52211262+MkshSahani@users.noreply.github.com>
2020-02-23 10:43:42 +0530

    Update two_layer_neural_network.py

--- two_layer_neural_network.py
+++ two_layer_neural_network.py
@@ -15,17 +15,11 @@ def init_weights(input_size, hidden_size):
 def forward_loss(X,y,weights):
 
     M1 = np.dot(X, weights['W1'])
-
     N1 = M1 + weights['B1']
-
     O1 = sigmoid(N1)
-    
     M2 = np.dot(O1, weights['W2'])
-
     P = M2 + weights['B2']    
-
     loss = np.mean(np.power(y - P, 2))
-
     forward_info = {}
     forward_info['X'] = X
     forward_info['M1'] = M1
@@ -38,41 +32,23 @@ def forward_loss(X,y,weights):
     return forward_info, loss
 
 def loss_gradients(forward_info ,weights):
-   
     dLdP = -2*(forward_info['y'] - forward_info['P'])
-    
     dPdM2 = np.ones_like(forward_info['M2'])
-
     dLdM2 = dLdP * dPdM2
-  
     dPdB2 = np.ones_like(weights['B2'])
-
     dLdB2 = (dLdP * dPdB2).sum(axis=0)
-    
     dM2dW2 = np.transpose(forward_info['O1'], (1, 0))
-    
     dLdW2 = np.dot(dM2dW2, dLdM2)
-
     dM2dO1 = np.transpose(weights['W2'], (1, 0)) 
-
     dLdO1 = np.dot(dLdM2, dM2dO1)
-    
     dO1dN1 = sigmoid(forward_info['N1']) * (1- sigmoid(forward_info['N1']))
-    
     dLdN1 = dLdO1 * dO1dN1
-    
     dN1dB1 = np.ones_like(weights['B1'])
-    
     dN1dM1 = np.ones_like(forward_info['M1'])
-    
     dLdB1 = (dLdN1 * dN1dB1).sum(axis=0)
-    
     dLdM1 = dLdN1 * dN1dM1
-    
     dM1dW1 = np.transpose(forward_info['X'], (1, 0)) 
-
     dLdW1 = np.dot(dM1dW1, dLdM1)
-
     loss_gradients: Dict[str, ndarray] = {}
     loss_gradients['W2'] = dLdW2
     loss_gradients['B2'] = dLdB2
@@ -81,17 +57,13 @@ def loss_gradients(forward_info ,weights):
     
     return loss_gradients
 
-def train(X_train, y_train ,n_iter = 1000,learning_rate = 0.01,hidden_size= 13,return_losses = False, return_weights  = False,return_scores = True,seed = 1):
-
-    if seed:
-        np.random.seed(seed)
+def train(X_train, y_train ,n_iter = 1000,learning_rate = 0.01,hidden_size= 13):
     weights = init_weights(X_train.shape[1], hidden_size=hidden_size)
-    for i in range(n_iter):
-        # forward info. 
+    for i in range(n_iter): 
         forward_info, loss = forward_loss(X_train, y_train, weights)
-        loss_grads = loss_gradients(forward_info, weights)
+        loss_gradient = loss_gradients(forward_info, weights)
         for key in weights.keys():
-            weights[key] -= learning_rate * loss_grads[key]
+            weights[key] -= learning_rate * loss_gradient[key]
     return weights 
 
 
@@ -105,23 +77,24 @@ def predict(X, weights):
     # print(f"{M1} : {N1} : {O1} : {M2} : {P}")
     return P
 
-x_train = [[3,4,5],[3,5,6],[4,6,7],[3,4,1],[4,3,2],[5,3,2]]
-y_train = [[9],[9],[10],[8],[9.5],[12]]
+if __name__ == '__main__':
+    x_train = [[3,4,5],[3,5,6],[4,6,7],[3,4,1],[4,3,2],[5,3,2]]
+    y_train = [[9],[9],[10],[8],[9.5],[12]]
 
 
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-print(x_train)
-print(y_train)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    print(x_train)
+    print(y_train)
 
-weights = train(x_train, y_train,hidden_size=3)
-print(weights)
+    weights = train(x_train, y_train,hidden_size=3)
+    print(weights)
 
-predict = predict(x_train, weights)
-print(predict)
-x_train_data = [i[0] for i in x_train]
-plt.title("2 layer neural network.")
-plt.scatter(x_train_data, y_train.flatten())
-plt.plot(x_train_data, predict.flatten(),'r')
-plt.show()
+    predict = predict(x_train, weights)
+    print(predict)
+    x_train_data = [i[0] for i in x_train]
+    plt.title("2 layer neural network.")
+    plt.scatter(x_train_data, y_train.flatten())
+    plt.plot(x_train_data, predict.flatten(),'r')
+    plt.show()
