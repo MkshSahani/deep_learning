285aedc9e52d96445e97c0b831957e6d074a6db9
MkshSahani <msco18333@gmail.com>
2020-07-21 17:48:24 +0530

    Neural Network Updated

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,1 +1,58 @@
-#  NeuralNetwork.py
+import numpy as np 
+
+
+
+class NeuralNetwork:
+    def __init__(self,  layers, loss, seed = 1) :
+        self.layers = layers
+        self.loss = loss
+        self.seed = seed
+        if seed:
+            for layer in self.layers:
+                setattr(layer, "seed", self.seed)        
+
+    def forward(self, x_batch):
+        x_out = x_batch
+        for layer in self.layers:
+            x_out = layer.forward(x_out)
+
+        return x_out
+
+    def backward(self, loss_grad):
+
+        grad = loss_grad
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+
+        return None
+
+    def train_batch(self,
+                    x_batch,
+                    y_batch):
+        '''
+        Passes data forward through the layers.
+        Computes the loss.
+        Passes data backward through the layers.
+        '''
+        
+        predictions = self.forward(x_batch)
+
+        loss = self.loss.forward(predictions, y_batch)
+
+        self.backward(self.loss.backward())
+
+        return loss
+    
+    def params(self):
+        '''
+        Gets the parameters for the network.
+        '''
+        for layer in self.layers:
+            yield from layer.params
+
+    def param_grads(self):
+        '''
+        Gets the gradient of the loss with respect to the parameters for the network.
+        '''
+        for layer in self.layers:
+            yield from layer.param_grads 
